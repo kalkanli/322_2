5481f2d9b8c9ef63c57976b8253eb43e12ae5d39
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-01-31 01:09:23 +0300

    this is taking unexpectedly long time

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 using namespace std;
 
-void *client_runner(void *params);
+void *client(void *params);
+
+pthread_mutex_t mutex; 
 
 int main(int argc, char const *argv[])
 {
     // <initialization_part>
+    pthread_mutex_init(&mutex, NULL);
+
     string input_file_path = argv[1];
     ifstream file(input_file_path);
 
@@ -20,14 +26,32 @@ int main(int argc, char const *argv[])
     getline(file, theater_name);
     getline(file, client_count_str);
     client_count = stoi(client_count_str);
-    cout << client_count << endl;
     string client[4];
     // <!initialization_part>
-
 
     // <teller_thread_start>
     //TODO TELLER
     // <!teller_thread_start>
+    
+    cout << theater_name << endl;
+    //cout << "OdaTiyatrosu" << endl;
+
+    //cout << theater_name.compare("OdaTiyatrosu") << endl;   
+
+    bool *seats;
+
+    if(theater_name.compare("OdaTiyatrosu\r") == 0) {
+        cout << "here" << endl;
+        seats = (bool*) calloc(60 ,sizeof(bool));
+    } else if(theater_name.compare("UskudarStudyoSahne\r") == 0) {
+        seats = (bool*) calloc(80 ,sizeof(bool));
+    } else if(theater_name.compare("KucukSahne\r") == 0) {
+        seats = (bool*) calloc(200 ,sizeof(bool));
+    }
+
+    for(int i=0; i<60; i++) {
+        cout << seats[i] << endl;
+    }
 
     // <client_thread_start>
     for(int i=0; i<client_count; i++) {
@@ -35,7 +59,6 @@ int main(int argc, char const *argv[])
         getline(file, client[1], ',');
         getline(file, client[2], ',');
         getline(file, client[3], '\n');
-        client_runner(client); // this line should be thread start.
     }
     // <!client_thread_start>
 
@@ -43,7 +66,7 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void *client_runner(void *params) 
+void *client(void *params) 
 {
     string *client = (string*) params;
     string client_name = client[0];
