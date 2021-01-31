eb694582db97c2c854180093f11fc09df0aa9fb9
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-01-31 21:15:45 +0300

    31st December

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,20 @@
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 using namespace std;
 
-void *client(void *params);
+pthread_mutex_t mutex;
+bool busy[3] = {false};
 
-pthread_mutex_t mutex; 
+void *client_runner(void* params);
+
 
 int main(int argc, char const *argv[])
 {
-    // <initialization_part>
+
+    pthread_t tellers[3];
     pthread_mutex_init(&mutex, NULL);
 
     string input_file_path = argv[1];
@@ -26,59 +30,69 @@ int main(int argc, char const *argv[])
     getline(file, theater_name);
     getline(file, client_count_str);
     client_count = stoi(client_count_str);
-    string client[4];
-    // <!initialization_part>
+    pthread_t client_thread_ids[client_count];
+    string clients[client_count];
 
-    // <teller_thread_start>
-    //TODO TELLER
-    // <!teller_thread_start>
-    
-    cout << theater_name << endl;
-    //cout << "OdaTiyatrosu" << endl;
-
-    //cout << theater_name.compare("OdaTiyatrosu") << endl;   
 
     bool *seats;
-
-    if(theater_name.compare("OdaTiyatrosu\r") == 0) {
-        cout << "here" << endl;
-        seats = (bool*) calloc(60 ,sizeof(bool));
-    } else if(theater_name.compare("UskudarStudyoSahne\r") == 0) {
-        seats = (bool*) calloc(80 ,sizeof(bool));
-    } else if(theater_name.compare("KucukSahne\r") == 0) {
-        seats = (bool*) calloc(200 ,sizeof(bool));
+    if (theater_name.compare("OdaTiyatrosu\r") == 0)
+    {
+        seats = (bool *)calloc(60, sizeof(bool));
+    }
+    else if (theater_name.compare("UskudarStudyoSahne\r") == 0)
+    {
+        seats = (bool *)calloc(80, sizeof(bool));
+    }
+    else if (theater_name.compare("KucukSahne\r") == 0)
+    {
+        seats = (bool *)calloc(200, sizeof(bool));
     }
 
-    for(int i=0; i<60; i++) {
-        cout << seats[i] << endl;
+    //client_count = 3;
+
+    for (int i = 0; i < client_count; i++)
+    {
+        getline(file, clients[i]);
+        pthread_create(&client_thread_ids[i], NULL, &client_runner, &clients[i]);
     }
 
-    // <client_thread_start>
-    for(int i=0; i<client_count; i++) {
-        getline(file, client[0], ',');
-        getline(file, client[1], ',');
-        getline(file, client[2], ',');
-        getline(file, client[3], '\n');
+    for(int i=0; i < client_count; i++) {
+        pthread_join(client_thread_ids[i], NULL);
     }
-    // <!client_thread_start>
 
+
+    free(seats);
+    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
 
-void *client(void *params) 
+
+
+void *client_runner(void* params)
 {
-    string *client = (string*) params;
-    string client_name = client[0];
-    int arrival_time = stoi(client[1]);
-    int service_time =  stoi(client[2]);
-    int seat_number =  stoi(client[3]);
+    string *client = (string *)params;
+    cout << client[0] << endl;
+    // string client_name = client[0];
+    // int arrival_time = stoi(client[1]);
+    // int service_time = stoi(client[2]);
+    // int seat_number = stoi(client[3]);
+    // cout << client_name << " " << arrival_time << service_time << seat_number << endl;
 
-    cout << seat_number << endl;
-    return 0;
+    pthread_mutex_lock(&mutex);
+    printf("Hello I am client\n" );
+    // %s, I would like the seat number %d", client_name.c_str(), seat_number
+    sleep(2);
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
+    //return 0;
 }
 
-void *teller(void *params) 
+
+
+
+void *teller(void *params)
 {
     return 0;
 }
