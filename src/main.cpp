5a2088a10d5ed2a484aab34d94b2de35f83cce63
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-01-31 23:52:01 +0300

    now it seems bit more easier

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,22 @@ using namespace std;
 pthread_mutex_t mutex;
 bool busy[3] = {false};
 sem_t available_tellers;
-struct job
-{
-    string client_name;
-    int seat_number;
-    int service_time;
-};
+sem_t ready_clients;
+struct job { string client_name; int seat_number; int service_time; };
 struct job *jobs;
+int ready_process = 0;
+bool *seats;
+int taken_seats = 0;
+int total_seats;
 
-void *client_runner(void* params);
+void *client_runner(void *params);
 void *teller_runner(void *params);
 
 int main(int argc, char const *argv[])
 {
 
     pthread_t tellers[3];
+    sem_init(&ready_clients, 0, 0);
     sem_init(&available_tellers, 0, 3);
     pthread_mutex_init(&mutex, NULL);
 
@@ -46,18 +47,20 @@ int main(int argc, char const *argv[])
 
     pthread_t teller_thread_ids[3];
 
-    bool *seats;
     if (theater_name.compare("OdaTiyatrosu\r") == 0)
     {
         seats = (bool *)calloc(60, sizeof(bool));
+        total_seats = 60;
     }
     else if (theater_name.compare("UskudarStudyoSahne\r") == 0)
     {
         seats = (bool *)calloc(80, sizeof(bool));
+        total_seats = 60;
     }
     else if (theater_name.compare("KucukSahne\r") == 0)
     {
         seats = (bool *)calloc(200, sizeof(bool));
+        total_seats = 200;
     }
 
     char a = 'A';
@@ -73,11 +76,13 @@ int main(int argc, char const *argv[])
         pthread_create(&client_thread_ids[i], NULL, &client_runner, &clients[i]);
     }
 
-    for(int i=0; i < client_count; i++) {
+    for (int i = 0; i < client_count; i++)
+    {
         pthread_join(client_thread_ids[i], NULL);
     }
 
-    for(int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         pthread_join(teller_thread_ids[i], NULL);
     }
 
@@ -87,44 +92,58 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-
-
-void *client_runner(void* params)
+void *client_runner(void *params)
 {
-    // TODO later can be changed.
-    string *client = (string*) params;
+    // later can be changed.
+    string *client = (string *)params;
     regex reg(",");
     sregex_token_iterator iter((*client).begin(), (*client).end(), reg, -1);
     sregex_token_iterator end;
     vector<string> vec(iter, end);
-    
+
     string client_name = vec[0];
     int arrival_time = stoi(vec[1]);
     int service_time = stoi(vec[2]);
     int seat_number = stoi(vec[3]);
-
     sleep(arrival_time);
 
+    /* CRITICAL SECTION */
     sem_wait(&available_tellers);
     pthread_mutex_lock(&mutex);
-    
-    struct job reservation = {
-        client_name,
-        seat_number,
-        service_time
-    };
-    
+    struct job reservation = { client_name, seat_number, service_time };
+    jobs[ready_process] = reservation;
     pthread_mutex_unlock(&mutex);
-    sem_post(&available_tellers);
-    
+    sem_post(&ready_clients);
+    /* CRITICAL SECTION */
+
     pthread_exit(NULL);
     //return 0;
 }
 
-
 void *teller_runner(void *params)
 {
-    char *teller = (char*) params;
-    cout << *teller << endl;
-    pthread_exit(0);
+    char *teller = (char *)params;
+    while (taken_seats != total_seats)
+    {
+        sem_wait(&ready_clients);
+        pthread_mutex_lock(&mutex);
+
+        struct job reservation = jobs[ready_process];
+        ready_process++;
+        
+        if(!seats[reservation.seat_number]) { // seat not taken
+            seats[reservation.seat_number] = true;
+        } else { // seat already taken.
+            for(int i=0; i<total_seats; i++) {
+                if(!seats[i]) {
+                    seats[i] = true;
+                }
+            }
+        }
+        taken_seats++;
+        sleep(reservation.service_time);
+
+
+    }
+
 }
